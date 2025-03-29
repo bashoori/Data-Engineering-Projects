2a20e60145f4f11ec58c6b4355e3ffe3c3ef174a
Bita Ashoori <68720409+bashoori@users.noreply.github.com>
2025-03-29 14:03:22 -0700

    Update Reading data from CSV.py

--- Working with different file formats/Reading data from CSV.py
+++ Working with different file formats/Reading data from CSV.py
@@ -28,12 +28,15 @@ df = df[['First Name', 'Last Name', 'Location ', 'City','State','Area Code']]
 df
 
 #Selecting rows using .iloc and .loc
+
+#loc() : loc() is label based data selecting method which means that we have to pass the name of the row or column which we want to select.
 # To select the first row
 df.loc[0]
 
 # To select the 0th,1st and 2nd row of "First Name" column only
 df.loc[[0,1,2], "First Name" ]
 
+#iloc() : iloc() is a indexed based selecting method which means that we have to pass integer index in the method to select specific row/column.
 # To select the 0th,1st and 2nd row of "First Name" column only
 df.iloc[[0,1,2], 0]
 
@@ -46,6 +49,7 @@ import numpy as np
 df=pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), columns=['a', 'b', 'c'])
 df
 
+#Let’s say we want to add 10 to each element in a dataframe:
 #applying the transform function
 df = df.transform(func = lambda x : x + 10)
 df
