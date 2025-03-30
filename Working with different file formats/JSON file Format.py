003e24093518a1308a43baca92a6d0da11f6e436
Bita Ashoori <68720409+bashoori@users.noreply.github.com>
2025-03-29 20:00:24 -0700

    Update JSON file Format.py

--- Working with different file formats/JSON file Format.py
+++ Working with different file formats/JSON file Format.py
@@ -1,4 +1,20 @@
+'''
 
+JSON (JavaScript Object Notation) is a lightweight data-interchange format. It is easy for humans to read and write.
+
+JSON is built on two structures:
+
+A collection of name/value pairs. In various languages, this is realized as an object, record, struct, dictionary, hash table, keyed list, or associative array.
+
+An ordered list of values. In most languages, this is realized as an array, vector, list, or sequence.
+
+JSON is a language-independent data format. It was derived from JavaScript, but many modern programming languages include code to generate and parse JSON-format data. It is a very common data format with a diverse range of applications.
+
+The text in JSON is done through quoted string which contains the values in key-value mappings within { }. It is similar to the dictionary in Python.
+
+Python supports JSON through a built-in package called json. To use this feature, we import the json package in Python script.
+
+'''
 import json
 
 person = {
@@ -14,6 +30,17 @@ person = {
 }
 
 #------------------------- Writing JSON to a File ----------------------------
+'''
+json.dump() method can be used for writing to JSON file.
+
+Syntax: json.dump(dict, file_pointer)
+
+Parameters:
+
+dictionary – name of the dictionary which should be converted to JSON object.
+file pointer – pointer of the file opened in write or append mode.
+
+'''
 
 with open('person.json', 'w') as f:  # writing JSON object
     json.dump(person, f)
